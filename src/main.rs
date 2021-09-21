1b83167e99469def3c3a867be22c6cef21d8d61f
Waine Oliveira Jr <waine.oliveira.jr@gmail.com>
2021-09-21 16:42:02 -0300

    Updated main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 pub mod common;
+pub mod stl_indexed;
 pub mod stl_reader;
 pub mod stl_triangle;
 
 fn main() {
     let filename = "fixture/cube.stl";
     let triangles = stl_reader::read_stl(filename);
-    for triangle in triangles {
-        println!("{}", triangle);
+    for triangle in triangles.iter() {
+        println!("1 {}", triangle);
+    }
+    let normalized_triangles = stl_triangle::normalize_triangles(&triangles);
+    for triangle in normalized_triangles.iter() {
+        println!("2 {}", triangle);
     }
 }

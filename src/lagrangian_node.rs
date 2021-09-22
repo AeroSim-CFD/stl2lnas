c07a36d170faa9ad2f307547ddd34c21c1e015b3
Waine Oliveira Jr <waine.oliveira.jr@gmail.com>
2021-09-22 20:29:24 -0300

    Updated lagrangia node

--- src/lagrangian_node.rs
+++ src/lagrangian_node.rs
@@ -1,7 +1,22 @@
+use crate::assert_almost_equal;
 use crate::common;
+use std::fmt;
 
-struct LagrangianNode {
-    pos: common::Vec3f,
-    area: f64,
-    normal: common::Vec3f,
+pub struct LagrangianNode {
+    pub pos: common::Vec3f,
+    pub normal: common::Vec3f,
+    pub area: f64,
+}
+
+impl LagrangianNode{
+    pub fn new(pos: common::Vec3f, normal: common::Vec3f, area: f64) -> LagrangianNode{
+        assert_almost_equal!(normal.norm(), 1f64, 1e-6f64);
+        return LagrangianNode{pos, normal, area};
+    }
+}
+
+impl fmt::Display for LagrangianNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        return write!(f, "pos {}, normal {}, area {}", self.pos, self.normal, self.area);
+    }
 }

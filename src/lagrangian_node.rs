d22f99f36218c96464ddd24b49985c20241e3ba7
Waine Oliveira Jr <waine.oliveira.jr@gmail.com>
2021-09-23 09:50:09 -0300

    Added serialization to lagrangian node

--- src/lagrangian_node.rs
+++ src/lagrangian_node.rs
@@ -1,8 +1,8 @@
 use crate::assert_almost_equal;
 use crate::common;
+use serde::Serialize;
 use std::convert::TryInto;
 use std::fmt;
-use serde::Serialize;
 
 #[derive(Serialize)]
 pub struct LagrangianNode {
@@ -11,37 +11,36 @@ pub struct LagrangianNode {
     pub area: f64,
 }
 
-fn demo<T, const N: usize>(v: Vec<T>) -> [T; N] {
-    v.try_into()
-        .unwrap_or_else(|v: Vec<T>| panic!("Expected a Vec of length {} but it was {}", N, v.len()))
-}
-
 pub const LAGRANGIAN_NODE_HEADER: [&str; 7] = [
-    "pos_x", 
-    "pos_y", 
-    "pos_z", 
-    "normal_x", 
-    "normal_y", 
-    "normal_z", 
-    "area"];
+    "pos_x", "pos_y", "pos_z", "normal_x", "normal_y", "normal_z", "area",
+];
 
-impl LagrangianNode{
-    pub fn new(pos: common::Vec3f, normal: common::Vec3f, area: f64) -> LagrangianNode{
+impl LagrangianNode {
+    pub fn new(pos: common::Vec3f, normal: common::Vec3f, area: f64) -> LagrangianNode {
         assert_almost_equal!(normal.norm(), 1f64, 1e-6f64);
-        return LagrangianNode{pos, normal, area};
+        return LagrangianNode { pos, normal, area };
     }
 
-    pub fn get_le_bytes(self) -> [u8; 28] {
-        let mut vec = [self.pos.to_le_bytes_as_f32(), self.normal.to_le_bytes_as_f32()].concat();
+    pub fn get_le_bytes(&self) -> [u8; 28] {
+        let mut vec = [
+            self.pos.to_le_bytes_as_f32(),
+            self.normal.to_le_bytes_as_f32(),
+        ]
+        .concat();
         vec.extend((self.area as f32).to_le_bytes());
 
-        return vec.try_into()
+        return vec
+            .try_into()
             .unwrap_or_else(|v: Vec<u8>| panic!("Expected a Vec of length 12, got {}", v.len()));
     }
 }
 
 impl fmt::Display for LagrangianNode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        return write!(f, "pos {}, normal {}, area {}", self.pos, self.normal, self.area);
+        return write!(
+            f,
+            "pos {}, normal {}, area {}",
+            self.pos, self.normal, self.area
+        );
     }
 }

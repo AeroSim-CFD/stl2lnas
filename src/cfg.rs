b63bea55b04f5c31edf97bcc510593ae3021e31e
Waine Oliveira Jr <waine.oliveira.jr@gmail.com>
2021-09-23 19:00:52 -0300

    Updated configs format

--- src/cfg.rs
+++ src/cfg.rs
@@ -4,22 +4,44 @@ use std::error::Error;
 use std::{fs, path, string::String};
 
 #[derive(PartialEq, Serialize, Deserialize)]
-pub struct ConversionConfigs {
-    pub path_stl: String,
-    pub folder_output: String,
-    pub size_x_lvl0: f32,
-    pub lvls_generate: Vec<u8>,
+pub struct ConfigsSTL {
+    pub filename: String,
+}
+
+#[derive(PartialEq, Serialize, Deserialize)]
+pub struct ConfigsOutput {
+    pub folder: String,
     pub save_csv: bool,
 }
 
-impl ConversionConfigs {
-    pub fn new(filename: &str) -> Result<ConversionConfigs, Box<dyn Error>> {
-        let f: ConversionConfigs = serde_yaml::from_reader(fs::File::open(filename)?)?;
+#[derive(PartialEq, Serialize, Deserialize)]
+pub struct ConfigsLvl0 {
+    pub x_size: f32,
+    pub min_area: f32,
+    pub max_area: f32,
+}
+
+#[derive(PartialEq, Serialize, Deserialize)]
+pub struct ConfigsConversion {
+    pub lvls_generate: Vec<u8>,
+    pub lvl0: ConfigsLvl0,
+}
+
+#[derive(PartialEq, Serialize, Deserialize)]
+pub struct Configs {
+    pub stl: ConfigsSTL,
+    pub conversion: ConfigsConversion,
+    pub output: ConfigsOutput,
+}
+
+impl Configs {
+    pub fn new(filename: &str) -> Result<Configs, Box<dyn Error>> {
+        let f: Configs = serde_yaml::from_reader(fs::File::open(filename)?)?;
         return Ok(f);
     }
 
     pub fn save_to_output_folder(&self) -> Result<(), Box<dyn Error>> {
-        let filename = path::Path::new(self.folder_output.as_str());
+        let filename = path::Path::new(self.output.folder.as_str());
         let filename = filename.join("cfg.yaml");
         let file = fs::File::create(filename.as_path())?;
         serde_yaml::to_writer(file, self)?;
--- src/main.rs
+++ src/main.rs
@@ -7,33 +7,34 @@ pub mod stl_triangle;
 pub mod utils;
 use std::path;
 
-use cfg::ConversionConfigs;
+use cfg::Configs;
 use lagrangian_node::LagrangianNode;
 use stl_triangle::TriangleSTL;
 
-const MAX_AREA_LVL0: f64 = 1f64;
-const MIN_AREA_LVL0: f64 = 0.2f64;
-
-fn get_min_max_area_for_lvl(lvl: u8) -> (f64, f64) {
+fn get_min_max_area_for_lvl(cfg: &Configs, lvl: u8) -> (f64, f64) {
     // Factor to wich divide lvl 0 area.
     // At each level, dist is divided by 2, so area is divided by 4 (2^2)
-    let area_factor = 4u32.pow(lvl as u32) as f64;
-    let (min_area, max_area) = (MIN_AREA_LVL0 / area_factor, MAX_AREA_LVL0 / area_factor);
-    return (min_area, max_area);
+    let area_factor = 4u32.pow(lvl as u32) as f32;
+    let (min_area, max_area) = (
+        cfg.conversion.lvl0.min_area / area_factor,
+        cfg.conversion.lvl0.max_area / area_factor,
+    );
+    return (min_area as f64, max_area as f64);
 }
 
-fn get_normalized_triangles(conv_cfg: &ConversionConfigs) -> Vec<TriangleSTL> {
-    let triangles = stl_reader::read_stl(conv_cfg.path_stl.as_str());
-    let triangles = stl_triangle::normalize_triangles(&triangles, conv_cfg.size_x_lvl0 as f64);
+fn get_normalized_triangles(cfg: &Configs) -> Vec<TriangleSTL> {
+    let triangles = stl_reader::read_stl(cfg.stl.filename.as_str());
+    let triangles =
+        stl_triangle::normalize_triangles(&triangles, cfg.conversion.lvl0.x_size as f64);
     return triangles;
 }
 
 fn generate_lagrangian_nodes_for_lvl(
-    conv_cfg: &ConversionConfigs,
+    cfg: &Configs,
     lvl: u8,
     orig_triangles: &Vec<TriangleSTL>,
 ) -> Vec<LagrangianNode> {
-    let (min_area, max_area) = get_min_max_area_for_lvl(lvl);
+    let (min_area, max_area) = get_min_max_area_for_lvl(cfg, lvl);
     let mut div_stl = stl_divider::DividerSTL::new(orig_triangles.clone());
     div_stl.divide_stl_by_area(max_area, min_area);
 
@@ -41,13 +42,9 @@ fn generate_lagrangian_nodes_for_lvl(
     return lagrangian_nodes;
 }
 
-fn save_nodes_for_lvl(
-    conv_cfg: &ConversionConfigs,
-    lvl: u8,
-    lagrangian_nodes: &Vec<LagrangianNode>,
-) {
-    let folder_path = path::Path::new(&conv_cfg.folder_output);
-    let (min_area, max_area) = get_min_max_area_for_lvl(lvl);
+fn save_nodes_for_lvl(cfg: &Configs, lvl: u8, lagrangian_nodes: &Vec<LagrangianNode>) {
+    let folder_path = path::Path::new(&cfg.output.folder);
+    let (min_area, max_area) = get_min_max_area_for_lvl(cfg, lvl);
 
     let lnas_filename = folder_path.join(format!("lvl{:02}.lnas", lvl));
     lagrangian_save::save_lagrangian_nodes_lnas(
@@ -58,7 +55,7 @@ fn save_nodes_for_lvl(
     )
     .unwrap_or_else(|e| println!("Saving lnas error for lvl {}. Error: {}", lvl, e));
 
-    if conv_cfg.save_csv {
+    if cfg.output.save_csv {
         let csv_filename = folder_path.join(format!("lvl{:02}.csv", lvl));
         lagrangian_save::save_lagrangian_nodes_csv(csv_filename.as_path(), lagrangian_nodes)
             .unwrap_or_else(|e| println!("Saving csv error for lvl {}. Error: {}", lvl, e));
@@ -67,16 +64,15 @@ fn save_nodes_for_lvl(
 
 fn main() {
     let filename_cfg = "fixture/convert_cube.yaml";
-    let conv_cfg = cfg::ConversionConfigs::new(filename_cfg).unwrap();
+    let cfg = cfg::Configs::new(filename_cfg).unwrap();
 
-    conv_cfg
-        .save_to_output_folder()
+    cfg.save_to_output_folder()
         .unwrap_or_else(|e| println!("Unable to save configs in its output folder. Error: {}", e));
 
-    let orig_triangles = get_normalized_triangles(&conv_cfg);
-    for lvl in conv_cfg.lvls_generate.iter() {
-        let lagrangian_nodes = generate_lagrangian_nodes_for_lvl(&conv_cfg, *lvl, &orig_triangles);
-        save_nodes_for_lvl(&conv_cfg, *lvl, &lagrangian_nodes);
+    let orig_triangles = get_normalized_triangles(&cfg);
+    for lvl in cfg.conversion.lvls_generate.iter() {
+        let lagrangian_nodes = generate_lagrangian_nodes_for_lvl(&cfg, *lvl, &orig_triangles);
+        save_nodes_for_lvl(&cfg, *lvl, &lagrangian_nodes);
         println!("Generated level {}!", lvl);
     }
 }

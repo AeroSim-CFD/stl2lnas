6f17be2fe0712a2bd855955367fd519bab9b5dc6
Waine Oliveira Jr <waine.oliveira.jr@gmail.com>
2021-09-23 09:50:48 -0300

    Cargo fmt

--- src/stl2lagrangian.rs
+++ src/stl2lagrangian.rs
@@ -5,24 +5,28 @@ use crate::stl_triangle::TriangleSTL;
 use std::collections::HashMap;
 use std::vec;
 
-struct NodeTriangles{
+struct NodeTriangles {
     pub pos: Vec3f,
     pub triangles: Vec<TriangleSTL>,
 }
 
-impl NodeTriangles{
+impl NodeTriangles {
     pub fn lagrangian_node(self, area_factor: f64) -> LagrangianNode {
         if self.triangles.len() == 0usize {
-            panic!("Node at {} is associated with no triangle, unable to generate lagrangian node.", self.pos);
+            panic!(
+                "Node at {} is associated with no triangle, unable to generate lagrangian node.",
+                self.pos
+            );
         }
-        let triangles_areas: Vec<f64> = self.triangles
-            .iter()
-            .map(|t| t.area())
-            .collect();
+        let triangles_areas: Vec<f64> = self.triangles.iter().map(|t| t.area()).collect();
 
-        let mut node_normal = Vec3f{x: 0f64, y: 0f64, z: 0f64};
+        let mut node_normal = Vec3f {
+            x: 0f64,
+            y: 0f64,
+            z: 0f64,
+        };
         let mut avg_areas = 0f64;
-        for (i, t) in self.triangles.iter().enumerate(){
+        for (i, t) in self.triangles.iter().enumerate() {
             avg_areas += t.area() / (self.triangles.len() as f64);
             let mut normal_sum = t.normal;
             normal_sum.multiply(triangles_areas[i]);
@@ -35,30 +39,35 @@ impl NodeTriangles{
     }
 }
 
-fn add_triange_to_hash_map(t_nodes_hash_map: &mut HashMap<Vec3f, NodeTriangles>, t: &TriangleSTL){
-    for p in [t.point0, t.point1, t.point2]{
-        t_nodes_hash_map.entry(p)
+fn add_triange_to_hash_map(t_nodes_hash_map: &mut HashMap<Vec3f, NodeTriangles>, t: &TriangleSTL) {
+    for p in [t.point0, t.point1, t.point2] {
+        t_nodes_hash_map
+            .entry(p)
             .and_modify(|tn| tn.triangles.push(t.clone()))
-            .or_insert(NodeTriangles{pos: p, triangles: vec![t.clone()]});
+            .or_insert(NodeTriangles {
+                pos: p,
+                triangles: vec![t.clone()],
+            });
     }
 }
 
-
-fn node_triangles_from_idx_stl(idx_stl: &IndexedSTL) -> Vec<NodeTriangles>{
+fn node_triangles_from_idx_stl(idx_stl: &IndexedSTL) -> Vec<NodeTriangles> {
     let mut t_nodes_hash_map: HashMap<Vec3f, NodeTriangles> = HashMap::new();
-    for t in idx_stl.triangles.iter(){
+    for t in idx_stl.triangles.iter() {
         add_triange_to_hash_map(&mut t_nodes_hash_map, t);
     }
 
-    return t_nodes_hash_map.into_iter()
+    return t_nodes_hash_map
+        .into_iter()
         .map(|(_k, node_triangles)| node_triangles)
         .collect();
 }
 
-pub fn stl2lagrangian(idx_stl: &IndexedSTL, area_factor: f64) -> Vec<LagrangianNode>{
+pub fn stl2lagrangian(idx_stl: &IndexedSTL, area_factor: f64) -> Vec<LagrangianNode> {
     let node_triangles = node_triangles_from_idx_stl(idx_stl);
 
-    return node_triangles.into_iter()
+    return node_triangles
+        .into_iter()
         .map(|nt| nt.lagrangian_node(area_factor))
         .collect();
-} 
+}
